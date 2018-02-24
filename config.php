82be2c083ae14479f4f32f1c3a728476afbedb10
instantdigitalsolution <36797438+instantdigitalsolution@users.noreply.github.com>
2018-02-25 01:35:52 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,59 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/cracko_data/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/cracko_data/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/cracko_data/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/cracko_data/system/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/cracko_data/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/cracko_data/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/cracko_data/system/config/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/cracko_data/image/');
+define('DIR_CACHE', 'D:/xampp/htdocs/cracko_data/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/cracko_data/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/cracko_data/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/cracko_data/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/cracko_data/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cracko_data');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+/*
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cracko.in/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cracko.in/');
+
+// DIR
+define('DIR_APPLICATION', '/home/cracko/public_html/catalog/');
+define('DIR_SYSTEM', '/home/cracko/public_html/system/');
+define('DIR_IMAGE', '/home/cracko/public_html/image/');
+define('DIR_LANGUAGE', '/home/cracko/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/cracko/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/cracko/public_html/system/config/');
+define('DIR_CACHE', '/home/cracko/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/cracko/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/cracko/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/cracko/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/cracko/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'cracko_main');
+define('DB_PASSWORD', 'w3cindia@123');
+define('DB_DATABASE', 'cracko_main');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+*/
+?>
